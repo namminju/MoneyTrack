d8512f5b195153ea6f24ed15b0758eb7cae49805
Heboen <cj2349@naver.com>
2025-04-07 23:32:11 +0900

    필터링 박스 요소 추가

--- src/components/Statistics/StatisticsFilterBox.vue
+++ src/components/Statistics/StatisticsFilterBox.vue
@@ -1,14 +1,40 @@
 <template>
-  <div class="filter-box"></div>
+  <div class="filter-box">
+    <div class="type__buttons">
+      <button
+        v-for="type in typeOptions"
+        :key="type"
+        @click="selectedType = type"
+        :class="{ active: selectedType === type }"
+      >
+        {{ type }}
+      </button>
+    </div>
+    <div class="period_buttons">
+      <button
+        v-for="period in periodOptions"
+        :key="period"
+        @click="selectedPeriod = period"
+        :class="{ active: selectedPeriod === period }"
+      >
+        {{ period }}
+      </button>
+    </div>
+    <button @click="applyFilter">적용</button>
+  </div>
 </template>
 
 <script setup>
 import { ref } from 'vue';
+
 const emit = defineEmits(['updateFilter']);
+const typeOptions = ['전체', '수입', '지츨'];
+const periodOptions = ['월간', '연간'];
+
 const selectedType = ref('전체');
 const selectedPeriod = ref('월간');
 
-const apply = () => {
+const applyFilter = () => {
   emit('updateFilter', {
     type: selectedType.value,
     period: selectedPeriod.period,
