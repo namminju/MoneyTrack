be089ecb59e2a452d01051b2cf05acb09de46a86
Heboen <cj2349@naver.com>
2025-04-08 09:06:39 +0900

    테스트

--- src/components/Statistics/Statistics.vue
+++ src/components/Statistics/Statistics.vue
@@ -1,36 +1,51 @@
-<!-- <template>
+<template>
   <div class="statistics-page">
     <Header />
     <StatisticsFilterBox @updateFilter="onFilterUpdate" />
 
-    <StatisticsGraph
+    <!-- <StatisticsGraph
       :filteredType="selectedType"
       :filteredPeriod="selectedPeriod"
     />
 
-    <CategoryList :filteredType="selectedType" />
+    <CategoryList :filteredType="selectedType" /> -->
+    <h2>📊 테스트용 통계 페이지</h2>
+
+    <p>🔹 전체 지출 합계: {{ statStore.totalAmount }} 원</p>
+
+    <ul>
+      <li v-for="item in statStore.categoryRatios" :key="item.category">
+        {{ item.category }}: {{ item.amount }}원 ({{ item.ratio }}%)
+      </li>
+    </ul>
   </div>
   <div>Statictics</div>
 </template>
 
 <script setup>
 import '@/css/statistics/statistics.css';
-import Header from '@/Header/Header.vue';
+import Header from '@/components/Header/Header.vue';
 import { ref } from 'vue';
 import StatisticsFilterBox from './StatisticsFilterBox.vue';
-import StatisticsGraph from './StatisticsGraph.vue';
-import CategoryList from './CategoryList.vue';
+// import StatisticsGraph from './StatisticsGraph.vue';
+// import CategoryList from './CategoryList.vue';
+import { useStatisticsStore } from '@/stores/useStatisticsStore';
 
+const store = useStatisticsStore();
 const selectedType = ref('전체');
 const selectedPeriod = ref('월간');
+
+onMounted(() => {
+  storeToRefs.fetchData();
+});
 
 const onFilterUpdate = ({ type, period }) => {
   selectedType.value = type;
   selectedPeriod.value = period;
 };
-</script> -->
+</script>
 
-<script setup>
+<!-- <script setup>
 import { onMounted } from 'vue';
 import { useStatisticsStore } from '@/stores/useStatisticsStore';
 
@@ -53,4 +68,4 @@ onMounted(() => {
       </li>
     </ul>
   </div>
-</template>
+</template> -->
