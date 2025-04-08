6b424d3083d7e9c0f01ddd57f8f97ec480e3f709
MJ <nampink2300@gmail.com>
2025-04-08 13:48:57 +0900

    feat: Add return category component

---
+++ src/components/Category/CategoryIcon.vue
@@ -0,0 +1,29 @@
+<template>
+  <i :class="iconClass"></i>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+  category: {
+    type: String,
+    required: true,
+  },
+});
+
+// 카테고리별 아이콘 매핑
+const categoryIconMap = {
+  식비: 'fa-solid fa-utensils',
+  교통: 'fa-solid fa-bus',
+  문화: 'fa-solid fa-palette',
+  급여: 'fa-solid fa-wallet',
+  용돈: 'fa-solid fa-coins',
+  기타: 'fa-solid fa-question',
+};
+
+// 해당 카테고리에 맞는 아이콘 클래스 반환
+const iconClass = computed(() => {
+  return categoryIconMap[props.category] || categoryIconMap['기타'];
+});
+</script>
