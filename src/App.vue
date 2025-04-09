f0e6d2baca0185d53e1c9e4cbc9b8718c7eaf71c
BaileyChoi <haileychoi9981@gmail.com>
2025-04-09 14:09:23 +0900

    feat: AlertProvider 컴포넌트 도입 및 전역 알림 시스템 연결
    - AlertProvider.vue에서 useAlert API를 provide 방식으로 노출
    - App.vue에서 AlertProvider로 앱 전체를 감싸고 slot으로 자식 컴포넌트 렌더
    - 각 컴포넌트에서 inject('useAlert')를 통해 전역 알림 호출 가능
    - Alert.vue와의 연결 완료로 사용자 정의 알림 완성

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
-  <Main/>
+  <AlertProvider>
+    <Main/>
+  </AlertProvider>
 </template>
 
 <script setup>
+  import AlertProvider from '@/components/providers/AlertProvider.vue';
   import Main from './components/Main/Main.vue';
 </script>
