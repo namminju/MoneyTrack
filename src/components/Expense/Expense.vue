94f8f96c10c95ac197f7c479cf037220b7a20743
MJ <nampink2300@gmail.com>
2025-04-09 16:35:25 +0900

    style: expense.module.css 분리

--- src/components/Expense/Expense.vue
+++ src/components/Expense/Expense.vue
@@ -1,4 +1,5 @@
 <template>
+  //적응형 스타일 적용
   <div :class="styles['expense-full-container']">
     <ExpenseCalendar
       :recordData="expenseStore.expenseList"
@@ -12,11 +13,14 @@
 </template>
 
 <script setup>
-import { reactive, ref, onMounted, watch } from 'vue';
+import { ref, onMounted, watch } from 'vue';
 import styles from '@/css/expense/expense.module.css';
 
+//components
 import ExpenseCalendar from './ExpenseCalender.vue';
 import ExpenseFilterContainer from './ExpenseFilterContainer.vue';
+
+// stores
 import { useExpenseStore } from '@/stores/expense';
 import { useCategoryStore } from '@/stores/category';
 
@@ -24,18 +28,21 @@ const expenseStore = useExpenseStore();
 const categoryStore = useCategoryStore();
 const user = ref('');
 
+//오늘 날짜로 default 설정
 const today = new Date();
 const selectedDate = ref(today);
 
+// user 변경되면 데이터 다시 받아오기
 watch(user, async (newUser) => {
   if (newUser && newUser.id) {
-    await expenseStore.fetchExpenseList(newUser.id);
+    await expenseStore.fetchExpenseList();
   }
 });
 
+//db 데이터 fetch
 onMounted(async () => {
   const mockUser = {
-    id: '2',
+    id: '1',
     name: '홍길동',
     email: 'hong@gmail.com',
     is_data: true,
@@ -47,8 +54,8 @@ onMounted(async () => {
 
     user.value = JSON.parse(sessionStorage.getItem('user'));
 
-    await categoryStore.fetchcategoryList(user.value.id);
-    await expenseStore.fetchExpenseList(user.value.id);
+    await categoryStore.fetchcategoryList();
+    await expenseStore.fetchExpenseList();
   } catch (e) {
     console.error('지출 데이터 불러오기 실패:', e);
   }
